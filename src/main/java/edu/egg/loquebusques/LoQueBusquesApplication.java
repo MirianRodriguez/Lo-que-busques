5ed3896225687a84d29a927b536767c30673aa69
Mirian Rodriguez <mirodriiguez8@gmail.com>
2022-06-02 19:28:05 -0300

    Creación del proyecto

---
+++ src/main/java/edu/egg/loquebusques/LoQueBusquesApplication.java
@@ -0,0 +1,13 @@
+package edu.egg.loquebusques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoQueBusquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoQueBusquesApplication.class, args);
+	}
+
+}
