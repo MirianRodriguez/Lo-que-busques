4fa36f6260715027daee980a418c73878c61cd58
Maia2019 <maiakrayeski@gmail.com>
2022-06-08 15:23:31 -0300

    Creacion de Servicio Emprendimiento y Domicilio: crear, modificar, eliminar

--- src/main/java/edu/egg/loquebusques/servicios/EmprendimientoServicio.java
+++ src/main/java/edu/egg/loquebusques/servicios/EmprendimientoServicio.java
@@ -1,5 +1,7 @@
 package edu.egg.loquebusques.servicios;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,11 +33,46 @@ public class EmprendimientoServicio (Emprendimiento emprendimientoDto) {
     emprendimiento.setHorario(emprendimientoDto.getHorario());
     emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
     emprendimiento.setCategorias(emprendimientoDto.getCategorias());
-    emprendimiento.setDomicilios(emprendimientoDto.getDomicilios());
+    emprendimiento.setDomicilios(emprendimientoDto.getDomicilio());
     emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
     emprendimiento.setArticulos(emprendimientoDto.getArticulos());
-     
+
 
     emprendimientoRepositorio.save(emprendimiento);
 }
+
+@Transactional
+public void actualizar(Emprendimiento emprendimientoDto) {
+
+    Emprendimiento emprendimiento = emprendimientoRepositorio.findById(emprendimientoDto.getId()).get();
+    
+    emprendimiento.setNombre(emprendimientoDto.getNombre());
+    emprendimiento.setDescripcion(emprendimientoDto.getDescripcion());
+    emprendimiento.setImagen(emprendimientoDto.getImagen());
+    emprendimiento.setTelefono(emprendimientoDto.getTelefono());
+    emprendimiento.setHorario(emprendimientoDto.getHorario());
+    emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
+    emprendimiento.setCategorias(emprendimientoDto.getCategorias());
+    emprendimiento.setDomicilios(emprendimientoDto.getDomicilio());
+    emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
+    emprendimiento.setArticulos(emprendimientoDto.getArticulos());
+
+    emprendimientoRepositorio.save(emprendimiento);
+}
+
+@Transactional(readOnly = true)
+    public List<Emprendimiento> obtenerTodos() {
+        return emprendimientoRepositorio.findAll();
+    }
+
+    @Transactional
+    public void eliminarPorId(Integer id) throws Exception {
+        if(emprendimientoRepositorio.referenciasEnArticulo(id)>0){
+            throw new Exception("No se puede eliminar porque hay registros asociados.");
+        }
+        
+    }
+
+
+
 }
