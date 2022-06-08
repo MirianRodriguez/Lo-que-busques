77aabf653e422cac33e223cceeb7c1aeb2c8c34e
Maia2019 <maiakrayeski@gmail.com>
2022-06-08 18:41:59 -0300

    Modificación en EmprendimientoServicio, eliminarPorId y obtenerPorId. Y emprendimientoRepositorio

--- src/main/java/edu/egg/loquebusques/servicios/EmprendimientoServicio.java
+++ src/main/java/edu/egg/loquebusques/servicios/EmprendimientoServicio.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.egg.loquebusques.entidades.Emprendimiento;
 import edu.egg.loquebusques.repositorios.EmprendimientoRepositorio;
 
-
 @Service
 public class EmprendimientoServicio {
 
@@ -19,52 +18,59 @@ public class EmprendimientoServicio {
     @Transactional
     public void crear(Emprendimiento emprendimientoDto) {
 
-    if (emprendimientoRepositorio.existsByNombre(emprendimientoDto.getNombre())){
-        throw new IllegalArgumentException("Ya existe un emprendimiento con ese nombre");
+        if (emprendimientoRepositorio.existsByNombre(emprendimientoDto.getNombre())) {
+            throw new IllegalArgumentException("Ya existe un emprendimiento con ese nombre");
+        }
+
+        Emprendimiento emprendimiento = new Emprendimiento();
+
+        emprendimiento.setNombre(emprendimientoDto.getNombre());
+        emprendimiento.setDescripcion(emprendimientoDto.getDescripcion());
+        emprendimiento.setImagen(emprendimientoDto.getImagen());
+        emprendimiento.setTelefono(emprendimientoDto.getTelefono());
+        emprendimiento.setHorario(emprendimientoDto.getHorario());
+        emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
+        emprendimiento.setCategorias(emprendimientoDto.getCategorias());
+        emprendimiento.setDomicilio(emprendimientoDto.getDomicilio());
+        emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
+        emprendimiento.setArticulos(emprendimientoDto.getArticulos());
+
+        emprendimientoRepositorio.save(emprendimiento);
     }
 
-    Emprendimiento emprendimiento = new Emprendimiento();
+    @Transactional
+    public void actualizar(Emprendimiento emprendimientoDto) {
 
+        Emprendimiento emprendimiento = emprendimientoRepositorio.findById(emprendimientoDto.getId()).get();
 
-    emprendimiento.setNombre(emprendimientoDto.getNombre());
-    emprendimiento.setDescripcion(emprendimientoDto.getDescripcion());
-    emprendimiento.setImagen(emprendimientoDto.getImagen());
-    emprendimiento.setTelefono(emprendimientoDto.getTelefono());
-    emprendimiento.setHorario(emprendimientoDto.getHorario());
-    emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
-    emprendimiento.setCategorias(emprendimientoDto.getCategorias());
-    emprendimiento.setDomicilios(emprendimientoDto.getDomicilio());
-    emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
-    emprendimiento.setArticulos(emprendimientoDto.getArticulos());
+        emprendimiento.setNombre(emprendimientoDto.getNombre());
+        emprendimiento.setDescripcion(emprendimientoDto.getDescripcion());
+        emprendimiento.setImagen(emprendimientoDto.getImagen());
+        emprendimiento.setTelefono(emprendimientoDto.getTelefono());
+        emprendimiento.setHorario(emprendimientoDto.getHorario());
+        emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
+        emprendimiento.setCategorias(emprendimientoDto.getCategorias());
+        emprendimiento.setDomicilio(emprendimientoDto.getDomicilio());
+        emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
+        emprendimiento.setArticulos(emprendimientoDto.getArticulos());
 
+        emprendimientoRepositorio.save(emprendimiento);
+    }
 
-    emprendimientoRepositorio.save(emprendimiento);
-}
-
-@Transactional
-public void actualizar(Emprendimiento emprendimientoDto) {
-
-    Emprendimiento emprendimiento = emprendimientoRepositorio.findById(emprendimientoDto.getId()).get();
-    
-    emprendimiento.setNombre(emprendimientoDto.getNombre());
-    emprendimiento.setDescripcion(emprendimientoDto.getDescripcion());
-    emprendimiento.setImagen(emprendimientoDto.getImagen());
-    emprendimiento.setTelefono(emprendimientoDto.getTelefono());
-    emprendimiento.setHorario(emprendimientoDto.getHorario());
-    emprendimiento.setFormasPago(emprendimientoDto.getFormasPago());
-    emprendimiento.setCategorias(emprendimientoDto.getCategorias());
-    emprendimiento.setDomicilios(emprendimientoDto.getDomicilio());
-    emprendimiento.setInicioActividades(emprendimientoDto.getInicioActividades());
-    emprendimiento.setArticulos(emprendimientoDto.getArticulos());
-
-    emprendimientoRepositorio.save(emprendimiento);
-}
-
-@Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public List<Emprendimiento> obtenerTodos() {
         return emprendimientoRepositorio.findAll();
     }
 
+    @Transactional(readOnly = true)
+    public Emprendimiento obtenerPorId(Integer id) {
+        return emprendimientoRepositorio.findById(id).get();
+    }
+
+    @Transactional
+    public void eliminarPorId(Integer id){
+        emprendimientoRepositorio.eliminarArticulosDelEmprendimiento(id);
+        emprendimientoRepositorio.deleteById(id);
+    }
+
 }
-
-
